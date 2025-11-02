8e5a9dc2e4d291689b7e4af8aed59c142f7f3551
cheikhteej <134007735+cheikhteej@users.noreply.github.com>
2025-11-02 18:05:27 +0000

    Update reame

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'dexchange SAAS',
-  description: 'Created with v0'
+  title: 'Dexchange SAAS',
+  description: 'Dexchange SAAS'
 }
 
 export default function RootLayout({
